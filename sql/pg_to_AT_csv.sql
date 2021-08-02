16908d238ba6776cbc978f8aa609f342f2b4397a
Masen Furer <m_github@0x26.net>
2021-08-01 20:52:04 -0700

    pg_to_AT_csv: use correct headers

--- sql/pg_to_AT_csv.sql
+++ sql/pg_to_AT_csv.sql
@@ -3,12 +3,12 @@ CREATE SEQUENCE rown;
 COPY (
   SELECT nextval('rown') as "No.",
          radio_id as "Radio ID",
-         UPPER(callsign) as Callsign,
-         CONCAT(INITCAP(name), UPPER(SUBSTRING(surname from 0 for 1))) as Name,
-         INITCAP(city) as City,
-         INITCAP(state) as State,
-         INITCAP(country) as Country,
-         last_heard as Remarks,
+         UPPER(callsign) as "Callsign",
+         CONCAT(INITCAP(name), UPPER(SUBSTRING(surname from 0 for 1))) as "Name",
+         INITCAP(city) as "City",
+         INITCAP(state) as "State",
+         INITCAP(country) as "Country",
+         last_heard as "Remarks",
          'Private Call' as "Call Type",
          'None' as "Call Alert"
   FROM pnwusers
