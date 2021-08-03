94ee34247df3fb6c3420f7c8fbd7baed58386184
Masen Furer <m_github@0x26.net>
2021-08-02 19:24:30 -0700

    update.sh: single script to generate and commit

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+$SCRIPTPATH/generate.sh
+git add .
+git commit -m "$(date +%Y-%m-%d) userdb update"
+git push origin main
