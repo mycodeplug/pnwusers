44e61ecc5fd808a0ba126bffc7350251f2a3b0c3
Masen Furer <m_github@0x26.net>
2021-08-02 19:30:24 -0700

    2021-08-02 userdb update

--- update.sh
+++ update.sh
@@ -4,5 +4,6 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 $SCRIPTPATH/generate.sh
 git add .
+git diff --cached
 git commit -m "$(date +%Y-%m-%d) userdb update"
 git push origin main
