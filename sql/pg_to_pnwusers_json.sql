e5d34dec11dba5dbd4665eae8cccab52fb2ad0f1
Masen Furer <m_github@0x26.net>
2021-08-06 19:33:53 -0700

    pg_to_pnwusers_json.sql produce valid JSON

--- sql/pg_to_pnwusers_json.sql
+++ sql/pg_to_pnwusers_json.sql
@@ -1,4 +1,4 @@
-COPY (SELECT ROW_TO_JSON(t) FROM (
+COPY (SELECT ARRAY_TO_JSON(ARRAY_AGG(t)) FROM (
     SELECT radio_id, callsign, name, city, state, country, last_heard
     FROM pnwusers
     ORDER BY last_heard DESC
