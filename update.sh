151d1207bb293e0bcb5666ee4cfb166946358033
Masen Furer <m_github@0x26.net>
2021-08-27 19:34:53 -0700

    update.sh: only push when generated data exceeds 3mb
    Want to avoid automatically uploading error messages if the db is down
    or otherwise.

--- update.sh
+++ update.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+MIN_DATA_SIZE=3000000
+
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 $SCRIPTPATH/generate.sh
+data_size=$(wc -c pnwusers* | tail -n 1 | cut -d ' ' -f 1)
+if [[ $data_size -lt $MIN_DATA_SIZE ]]; then
+  echo "Total bytes generated, $data_size, is less than threshold $MIN_DATA_SIZE"
+  git reset --hard origin/main
+  exit 1
+fi
 git add .
-git diff --cached
-git commit -m "$(date +%Y-%m-%d) userdb update"
+git commit <<< "$(date +%Y-%m-%d) userdb update\n\n$(git diff --summary --stat --no-patch)"
 git push origin main
